2e0f4f200eddd190f7196c416f353cb15c62208d
Anvesha jain <anveshajain1436@gmail.com>
2025-08-01 15:13:15 +0530

    added Exponential file

---
+++ Exponential.py
@@ -0,0 +1,19 @@
+import time
+
+def exponential_time(n):
+    print(f"\nO(2ⁿ) for n={n}")
+    def helper(k):
+        if k==0:
+            return 
+        helper(k-1)
+        helper(k-1)
+
+    start = time.time()
+    helper(n)
+    end = time.time()
+
+    print(f"Time taken for n = {n} is {end - start:.6f} seconds")
+
+exponential_time(20)
+exponential_time(25)
+exponential_time(30)
