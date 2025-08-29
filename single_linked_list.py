5aae27c05b54535696be3030ba839660922823af
Anvesha jain <anveshajain1436@gmail.com>
2025-08-29 14:48:19 +0530

    added single linked list file

---
+++ single_linked_list.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+linked_list = LinkedList()
+linked_list.add(10)
+linked_list.add(20)
+linked_list.add(30)
+linked_list.display()
